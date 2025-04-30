c9c500bc31d44e7c2d3e0edbd6dfc920e12d4ad8
Ragini <padwalragini20@gmail.com>
2025-04-30 09:50:57 +0530

    chages made in branch

--- SELL/Selenium/Selenium/Selenium/Selenium/Car_loan.java
+++ SELL/Selenium/Selenium/Selenium/Selenium/Car_loan.java
@@ -3,10 +3,16 @@ package Selenium.Selenium;
 import org.testng.annotations.Test;
 
 public class Car_loan {
-	@Test  
-	 public void car_loan()  
-	 {  
-	     System.out.println("Car Loan");  
-	     System.out.println("Vehicle Loan");  
-}
+	@Test
+	public void car_loan() {
+		System.out.println("Car Loan");
+		System.out.println("Vehicle Loan");
+	}
+
+	{
+		System.out.println("POSTJIRA1");
+		System.out.println("POSTJIRA2");
+		System.out.println("POSTJIRA3");
+		System.out.println("POSTJIRA4");
+	}
 }
